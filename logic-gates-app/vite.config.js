bc8362a541dc91f6313849dacc83247cdd41bd61
Samuel Yew Han Sen <samuelyew98@gmail.com>
2025-04-09 22:53:53 +0800

    added the base attribute in vite.config.js to re-route everything to use router

--- logic-gates-app/vite.config.js
+++ logic-gates-app/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   define: {
     'process.env.BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL || 'http://localhost:8080'),
